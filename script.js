89645580c5c802d51b34017ea7802287aa5e06eb
Basanta saru <basantawithgpt@gmail.com>
2025-11-01 05:08:05 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("bookBtn").addEventListener("click", function() {
+  // Replace this path with your actual file path or URL later
+  const bookPath = "path/to/Haribodhini_Ekadashi_First_Version.pdf";
+  window.open(bookPath, "_blank");
+});
+
+document.getElementById("bookBtn").addEventListener("click", function() {
+  // direct download
+  const link = document.createElement("a");
+  link.href = "https://basantasaru.github.io/Haribodhini_Ekadashi_First_Version.pdf";
+  link.download = "Haribodhini_Ekadashi_First_Version.pdf";
+  link.click();
+});
